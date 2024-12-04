f203c8df130d350621389dc4c732258d85a78243
AleksanderBjelk <bjelkaleksander@gmail.com>
2024-12-04 10:02:29 +0100

    pushar video till startsida

--- react_template/my-app/src/JSX/main.jsx
+++ react_template/my-app/src/JSX/main.jsx
@@ -4,17 +4,29 @@ import ProductGridCarousel from "../JSX/ProductGridCarousel.jsx";
 function MainPage() {
     return (
         <div className="main">
-            <div className="frontPagePicture">
+            <video id="video" loop muted autoPlay controls>
+                <source
+                    src="/media/startvideo.mp4"
+                    alt="Video på kläder butiken erbjuder för jul"
+                    type="video/mp4"
+                ></source>
+                <source
+                    src="/media/startvideo.webm"
+                    alt="Video på kläder butiken erbjuder för jul"
+                    type="video/webm"
+                ></source>
+            </video>
+            {/* <div className="frontPagePicture">
                 <img src="/media/startbild4.png" alt="Example" />
                 <div>
                     <h1 className="slogan-text">Make a statement out of yourself</h1>
                 </div>
-            </div>
+            </div>  */}
             <div className="upperMainText">
                 <h1>NEW IN WINTER SEASON</h1>
             </div>
             <ProductGridCarousel />
-              <div className="lowerMainTextContainer">
+            <div className="lowerMainTextContainer">
                 <div className="lowerMainText">
                     <h2>OUTLET SALE UNTIL 24/11</h2>
                     <h2>FREE DELIVERY TO STORE</h2>
