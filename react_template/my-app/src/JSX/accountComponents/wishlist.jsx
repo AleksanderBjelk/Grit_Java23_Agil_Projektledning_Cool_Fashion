1b6ffe36363cfb30f9760d2fc2eaccef7ac60966
AleksanderBjelk <bjelkaleksander@gmail.com>
2024-12-05 13:00:03 +0100

    namnändring

---
+++ react_template/my-app/src/JSX/accountComponents/wishlist.jsx
@@ -0,0 +1,18 @@
+import "../../CSS/resell.css";
+import WishlistCard from "./WishlistCard.jsx";
+
+function WishlistPage() {
+    return (
+        <div className="resell">
+            <div className="header">
+                <div className="content">
+                <h1>Din Wishlist</h1>
+            </div>
+            <div className="cards">
+                <WishlistCard />
+                </div>
+            </div>
+        </div>
+    );
+}
+export default WishlistPage;
